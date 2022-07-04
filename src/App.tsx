30a591aacf73a5bf6d54df6fdd5767ae31b11aca
haneulcha <zr420cha@gmail.com>
2022-07-04 20:45:59 +0900

    Add: Image upload

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,40 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
+  const [imgFile, setImgFile] = useState("");
+
+  const onImgChange = (evt: React.ChangeEvent<HTMLInputElement>) => {
+    const img = evt.target.files?.[0];
+    if (!img) return;
+
+    const reader = new FileReader();
+    reader.onload = (e) => {
+      setImgFile(e.target?.result as string);
+    };
+    reader.readAsDataURL(img);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <header>이미지를 텍스트로</header>
+      <main>
+        <form onSubmit={() => console.log("on submit")}>
+          <label htmlFor="img">업로드</label>
+          <input id="img" type="file" accept="image/*" onChange={onImgChange} />
+        </form>
+
+        <div className="uploaded-img__wrapper">
+          {imgFile && (
+            <img
+              className="uploaded-img__img"
+              src={imgFile}
+              alt="유저가 업로드한 이미지"
+            />
+          )}
+        </div>
+      </main>
     </div>
   );
 }
